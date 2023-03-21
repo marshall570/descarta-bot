227302911d10a3701590e742a185d0f6d2d148b1
Gustavo Fernandes <gustavosergio2001@gmail.com>
2023-03-21 16:10:30 -0300

    Quase funcional

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import os
 import pandas
 from dotenv import load_dotenv
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 
 
@@ -29,10 +29,78 @@ def checar_tombo(tombo):
     return True
 
 
+def checar_paginas(item):
+    try:
+        driver.find_element(
+            By.XPATH,
+            f'//*[contains(text(),{item})]'
+        )
+    except NoSuchElementException:
+        return False
+    return True
+
+
+def checar_livro(links, tombo):
+    for item in links:
+        try:
+            driver.get(item)
+
+            driver.find_element(
+                By.XPATH,
+                f'//td[text()={tombo}]'
+            )
+        except NoSuchElementException:
+            continue
+        return True
+
+
+def varrer_dados(tombo):
+    titulo = str(driver.find_element(
+        By.XPATH,
+        '//*[contains(text(),"Título:")]/following-sibling::*'
+    ).get_attribute('textContent').strip().replace('\n', ''))
+
+    autor = str(driver.find_element(
+        By.XPATH,
+        '//*[contains(text(),"Personal name:")]/following-sibling::*'
+    ).get_attribute('textContent').strip().replace('\n', ''))
+
+    editora = str(driver.find_element(
+        By.XPATH,
+        '//*[contains(text(),"Name of publisher, distributor, etc.:")]/following-sibling::*'
+    ).get_attribute('textContent').strip().replace('\n', ''))
+
+    ano = str(driver.find_element(
+        By.XPATH,
+        '//*[contains(text(),"Date of publication, distribution, etc.:")]/following-sibling::*'
+    ).get_attribute('textContent').strip().replace('\n', ''))
+
+    isbn = str(driver.find_element(
+        By.XPATH,
+        '//*[contains(text(),"International Standard Book Number:")]/following-sibling::*'
+    ).get_attribute('textContent').strip())
+
+    item = str(tombo)
+
+    exemplar = str(driver.find_element(
+        By.XPATH,
+        f'//td[text()={item}]/following-sibling::*'
+    ).get_attribute('textContent').replace('\n', ''))
+
+    return [titulo, autor, editora, ano, isbn, tombo, exemplar]
+
+
+lista = [['TITULO', 'AUTOR', 'EDITORA', 'ANO', 'ISBN', 'TOMBO', 'EXEMPLAR']]
+
+# DRIVER
+options = Options()
+# options.add_argument('-headless')
+driver = webdriver.Firefox(options=options)
+driver.implicitly_wait(5)
+
 try:
     # LISTA
-    cods = ['1963']
-    # cods = ['01258', '6154', '01994', '1963']
+    tombos = pandas.read_csv('tombos.csv', dtype=str).squeeze('columns')
 
     # LOGIN
     load_dotenv()
@@ -49,12 +117,6 @@ try:
     selector_botao_pesquisar = 'body > table:nth-child(3) > tbody > tr:nth-child(3) > td:nth-child(5) > font.primary > form:nth-child(3) > table > tbody > tr:nth-child(2) > td > input.button'
     selector_guia_pesquisa = 'body > table:nth-child(3) > tbody > tr:nth-child(3) > td:nth-child(2) > font > a:nth-child(4)'
 
-    # DRIVER
-    # options = Options()
-    # options.headless = True
-    driver = webdriver.Firefox()
-    driver.implicitly_wait(3)
-
     # ROTINA
     driver.get('http://201.55.32.179/openbiblio/shared/loginform.php')
     driver.find_element(By.CSS_SELECTOR, selector_usuario).send_keys(usuario)
@@ -62,58 +124,44 @@ try:
     driver.find_element(By.CSS_SELECTOR, selector_botao_login).click()
     driver.find_element(By.CSS_SELECTOR, selector_guia_catalogo).click()
 
-    lista = [['TITULO', 'AUTOR', 'EDITORA', 'ANO', 'ISBN', 'TOMBO', 'EXEMPLAR']]
-    for item in cods:
+    for item in tombos:
+        # print(item)
         driver.find_element(
             By.CSS_SELECTOR, selector_codigo_de_barras).send_keys(item)
         driver.find_element(By.CSS_SELECTOR, selector_botao_pesquisar).click()
 
         if checar_lista():
-            titulo = str(driver.find_element(
-                By.XPATH,
-                '//*[contains(text(),"Título:")]/following-sibling::*'
-            ).get_attribute('textContent').strip().replace('\n', ''))
-            autor = str(driver.find_element(
-                By.XPATH,
-                '//*[contains(text(),"Personal name:")]/following-sibling::*'
-            ).get_attribute('textContent').strip().replace('\n', ''))
-            editora = str(driver.find_element(
-                By.XPATH,
-                '//*[contains(text(),"Name of publisher, distributor, etc.:")]/following-sibling::*'
-            ).get_attribute('textContent').strip().replace('\n', ''))
-            ano = str(driver.find_element(
-                By.XPATH,
-                '//*[contains(text(),"Date of publication, distribution, etc.:")]/following-sibling::*'
-            ).get_attribute('textContent').strip().replace('\n', ''))
-            isbn = str(driver.find_element(
-                By.XPATH,
-                '//*[contains(text(),"International Standard Book Number:")]/following-sibling::*'
-            ).get_attribute('textContent').strip())
-            tombo = str(item)
-            exemplar = str(driver.find_element(
-                By.XPATH,
-                f'//*[contains(text(),{item})]/following-sibling::*'
-            ).get_attribute('textContent').replace('\n', ''))
+            lista.append(varrer_dados(item))
 
         else:
-            # /html/body/table[3]/tbody/tr[3]/td[5]/font[1]/table/tbody/tr[3]/td[1]/font
-            celula = driver.find_element(By.XPATH, f'//tr[. /td/*[contains(text(),{item})]]/preceding::tbody')
-            print(celula.get_attribute('textContent'))
-            links = celula.find_elements(By.XPATH, 'a')
+            if checar_paginas(item):
+                livros = driver.find_elements(
+                    By.XPATH,
+                    f'//a[. /img[@src="../images/book.gif"]]'
+                )
 
-            for i in links:
-                print(i.get_attribute('textContent'))
+                links = []
 
-            
+                for link in livros:
+                    links.append(link.get_attribute('href'))
+                
+                if checar_livro(links, item):
+                    lista.append(varrer_dados(item))
+                    pass                
 
-        lista.append([titulo, autor, editora, ano, isbn, tombo, exemplar])
+            else:
+                lista.append(['NÃO ENCONTRADO', '', '', '', '', item, ''])
+
         driver.find_element(By.CSS_SELECTOR, selector_guia_pesquisa).click()
 
     dataframe = pandas.DataFrame(lista)
-    dataframe.to_csv('teste.csv', index=False, header=False)
+    dataframe.to_csv('descartes.csv', index=False, header=False)
 
+    driver.find_element(By.CSS_SELECTOR, selector_botao_logout).click()
     driver.quit()
 
 except Exception as error:
+    dataframe = pandas.DataFrame(lista)
+    dataframe.to_csv('descartes.csv', index=False, header=False)
     driver.quit()
     print(error)
